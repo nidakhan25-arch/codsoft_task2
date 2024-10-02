bc34b8f9274b4db41355d625407c2a20b6e7c6b5
NIda khan <knida131@gmail.com>
2024-10-02 13:22:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+(document).ready(function(){
+
+    $('menu').click(function(){
+        $(this).toggleclass('fa-times');
+        $('header').toggleclass('toggle');
+
+    });
+$(window).on('scroll-load' ,function(){
+
+    $('#menu').removeclass('fa-times');
+    $('header').removeclass('toggle');
+
+
+
+
+});
+
+//smooth scrolling
+$('a[href*="#"]').on('click',function(e){
+e.preventDefault();
+$('html,body').animate({
+screenTop:$($(this).attr('href')).offset().top,
+
+},
+500,
+'linear'
+);
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
